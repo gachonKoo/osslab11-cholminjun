d37d2a519a9d609c9ded13fe536e13ccd52028a5
cholminjun <rhynogore101@gmail.com>
2025-11-16 18:00:33 +0900

    158135805301

--- geo/utils.py
+++ geo/utils.py
@@ -1,8 +1,12 @@
+# geo/utils.py
 import math
-def circle(r):
-    area = pi * r^2
-    return area
 
 def pythagoras(a, b):
-    c = sqrt(a^2 + b^2)
+    # 피타고라스 정리: c = sqrt(a^2 + b^2)
+    c = math.sqrt(a**2 + b**2)  # ^ 말고 ** 사용!
     return c
+
+def circle(r):
+    # 원 넓이: π r^2
+    area = math.pi * (r**2)
+    return area
